ebc0eb047a59e501c419296f10393b3c93a8c6fc
LAH <00957047@email.ntou.edu.tw>
2024-11-26 22:48:23 +0800

    Set homepage buttons' positions

--- pdf_reader/lib/main.dart
+++ pdf_reader/lib/main.dart
@@ -1,125 +1,208 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDF Reader',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'PDFReader Home Page'),
+      title: 'File Manager',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FileManagerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FileManagerScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _FileManagerScreenState createState() => _FileManagerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FileManagerScreenState extends State<FileManagerScreen> {
+  List<Map<String, dynamic>> items = [];
 
-  void _incrementCounter() {
+  bool showAddOptions = false; // 控制圓形按鈕的顯示狀態
+
+  void _addFolder(String folderName) {
+    // TODO
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      items.add({"name": folderName, "type": "folder"});
+    });
+  }
+
+  void _addFile(String fileName) {
+    setState(() {
+      items.add({"name": fileName, "type": "file"});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('所有檔案'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Column(children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return ListTile(
+                    leading: Icon(
+                      item['type'] == 'folder'
+                          ? Icons.folder
+                          : Icons.insert_drive_file,
+                      color: item['type'] == 'folder'
+                          ? Colors.orange
+                          : Colors.blue,
+                    ),
+                    title: Text(item['name']),
+                    onTap: () {
+                      if (item['type'] == 'folder') {
+                        // TODO
+
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("打開資料夾：${item['name']}"),
+                        ));
+                      } else {
+                        // TODO
+
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("打開檔案：${item['name']}"),
+                        ));
+                      }
+                    },
+                  );
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ]),
+
+          // 新增檔案的按鈕
+          if (showAddOptions)
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 200),
+              bottom: 75,
+              right: 16,
+              width: 135,
+              child: TextButton.icon(
+                label: Text('新增檔案', style: TextStyle(color: Colors.white)),
+                icon: Icon(
+                  Icons.note_add,
+                  color: Colors.white,
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey.shade700,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () async {
+                  String? fileName =
+                      await _showInputDialog(context, "新增檔案", "檔案名稱");
+                  if (fileName != null && fileName.trim().isNotEmpty) {
+                    _addFile(fileName.trim());
+                  }
+                  setState(() {
+                    showAddOptions = false; // 點擊後收起按鈕
+                  });
+                },
+              ),
+            ),
+
+          // 新增資料夾的按鈕
+          if (showAddOptions)
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 200),
+              bottom: 120,
+              right: 16,
+              width: 135,
+              child: TextButton.icon(
+                label: Text('新增資料夾', style: TextStyle(color: Colors.white)),
+                icon: Icon(
+                  Icons.create_new_folder,
+                  color: Colors.white,
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.grey.shade700,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                onPressed: () async {
+                  String? folderName =
+                      await _showInputDialog(context, "新增資料夾", "資料夾名稱");
+                  if (folderName != null && folderName.trim().isNotEmpty) {
+                    _addFolder(folderName.trim());
+                  }
+                  setState(() {
+                    showAddOptions = false; // 點擊後收起按鈕
+                  });
+                },
+              ),
+            ),
+        ],
+      ),
+
+      // + 的按鈕
+      floatingActionButton: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          FloatingActionButton(
+            heroTag: "main_add",
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            backgroundColor:
+                showAddOptions ? Colors.grey.shade500 : Colors.grey.shade700,
+            onPressed: () {
+              setState(() {
+                showAddOptions = !showAddOptions; // 切換狀態
+              });
+            },
+            child: Icon(
+              showAddOptions ? Icons.close : Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<String?> _showInputDialog(
+      BuildContext context, String title, String hint) async {
+    TextEditingController inputController = TextEditingController();
+    return showDialog<String>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: TextField(
+            controller: inputController,
+            decoration: InputDecoration(hintText: hint),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(null);
+              },
+              child: Text("取消"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(inputController.text);
+              },
+              child: Text("確認"),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
 }
